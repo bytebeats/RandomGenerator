72500d8269e028895f74990090b761b5840fe54b
panchen <panchen@tigerbrokers.com>
2021-09-01 11:58:09 +0800

    Initial commit

---
+++ app/src/main/java/me/bytebeats/generator/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.bytebeats.generator.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
